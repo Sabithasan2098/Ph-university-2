8873d78d5cb967809d187f96c387c2f45739da04
Sabithasan2098 <sabithasan2098@gmail.com>
2024-11-03 19:07:31 +0600

    delete user and student use transection & rollback

--- src/app/modules/students/students.service.ts
+++ src/app/modules/students/students.service.ts
@@ -1,4 +1,7 @@
+import mongoose from "mongoose";
 import { StudentModelSchema } from "./students.model";
+import { appError } from "../../error/custom.appError";
+import { userModelSchema } from "../user/user.model";
 
 // get all students------------------------------------->
 export const getAllStudentsFromDB = async () => {
@@ -18,9 +21,42 @@ export const getAStudentDataByIdFromDB = async (id: string) => {
 // get a single student data---------------------------->
 // actually we don't delete data just update a field
 export const deleteAStudentDataByIdFromDB = async (id: string) => {
-  const result = await StudentModelSchema.updateOne(
-    { id },
-    { isDeleted: true },
-  );
-  return result;
+  const session =
+    await mongoose.startSession(); /*start session to transection*/
+  try {
+    session.startTransaction(); /*start transection */
+    const deleteStudent = await StudentModelSchema.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      {
+        new: true,
+        session,
+      } /*session phathano holo update er jonno e rokom vabe phathate hbe */,
+    );
+
+    if (!deleteStudent) {
+      throw new appError(400, "faield to delete student");
+    }
+
+    // delete user---------------------->
+    const deleteUser = await userModelSchema.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      {
+        new: true,
+        session,
+      } /*session phathano holo update er jonno e rokom vabe phathate hbe */,
+    );
+
+    if (!deleteUser) {
+      throw new appError(400, "faield to delete user");
+    }
+
+    await session.commitTransaction(); /*successful hole commit kora*/
+    await session.endSession(); /*session send kora*/
+    return deleteStudent;
+  } catch (error) {
+    await session.abortTransaction(); /*faield hole transection off kora*/
+    await session.endSession(); /*session send kora*/
+  }
 };
